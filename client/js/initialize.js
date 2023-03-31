20d8cffef474ab99f985357a9f28c8c44162d9fd
itsericchau <chun.wing.chau@gmail.com>
2023-03-31 16:50:36 +1100

    removing random question generation

--- client/js/initialize.js
+++ client/js/initialize.js
@@ -21,12 +21,12 @@ function getQuestion(value = null) {
         renderQuizQuestion()
       })
   } else {
-    axios.get('https://jservice.io/api/random')
-      .then(res => res.data)
-      .then(question => {
-        state.question = question[0]
-        renderQuizQuestion()
-      })
+    // axios.get('https://jservice.io/api/random')
+    //   .then(res => res.data)
+    //   .then(question => {
+    //     state.question = question[0]
+    //     renderQuizQuestion()
+    //   })
   }
 }
 
